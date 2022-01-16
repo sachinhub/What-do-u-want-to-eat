d74eab234264ed36d065529a1d09afbc88820a98
Sachin <sachin@funbook.local>
2022-01-16 16:15:17 +0530

    first change

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="What do you want eat?" />
+        <Header title="What do you want eat? - a project by Raghav and Sachin" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
